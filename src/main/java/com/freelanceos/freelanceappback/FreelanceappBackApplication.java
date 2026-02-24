37be7c34730ad5c56400633e8d50d0b276cb56c7
Benjamin <benjamin.martel.98@gmail.com>
2026-02-24 23:35:55 +0100

    init blank springboot project

---
+++ src/main/java/com/freelanceos/freelanceappback/FreelanceappBackApplication.java
@@ -0,0 +1,13 @@
+package com.freelanceos.freelanceappback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelanceappBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreelanceappBackApplication.class, args);
+    }
+
+}
